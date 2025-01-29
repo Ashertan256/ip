0a0e8ba4c7734d721c02ea54de230fcf19b1d81c
Ashertan256 <ashertan02@gmail.com>
2025-01-30 01:55:27 +0800

    Week 3. Level 2

--- src/main/java/Alex.java
+++ src/main/java/Alex.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Alex {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println("Hello! I'm Alex");
         System.out.println("What can I do for you?");
@@ -12,15 +14,31 @@ public class Alex {
         do {
             input = scanner.nextLine();
 
-            if (!input.equalsIgnoreCase("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println(input); // Echo the user input
-                System.out.println("____________________________________________________________");
+            if (input.equalsIgnoreCase("list")) {
+                if (taskCount == 0) {
+                    System.out.println("No tasks");
+                } else {
+                    System.out.println("    ____________________________________________________________");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println("    "+(i + 1) + ". " + tasks[i]);
+                    }
+                    System.out.println("    ____________________________________________________________");
+                }
+            } else if (!input.equalsIgnoreCase("bye")) {
+                if(taskCount < 100){
+                    tasks[taskCount] = input;
+                    taskCount++;
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("    added: " + input);
+                    System.out.println("    ____________________________________________________________");
+                }
+                
             }
         } while (!input.equalsIgnoreCase("bye"));
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
         scanner.close();
     }
 }
