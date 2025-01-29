eec22d047845ff32568e61a6192c7990502386f7
Ashertan256 <ashertan02@gmail.com>
2025-01-30 01:50:06 +0800

    Week 3. Level 1

--- src/main/java/Alex.java
+++ src/main/java/Alex.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class Alex {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello! I'm Alex");
         System.out.println("What can I do for you?");
         System.out.println();
+
+        String input;
+        do {
+            input = scanner.nextLine();
+
+            if (!input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(input); // Echo the user input
+                System.out.println("____________________________________________________________");
+            }
+        } while (!input.equalsIgnoreCase("bye"));
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+        scanner.close();
     }
 }
