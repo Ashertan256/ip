2fcc2a0a2a91ae0b29838bbf662114246c13e86a
Ashertan256 <ashertan02@gmail.com>
2025-01-30 02:27:47 +0800

    Week 3. Level 3

--- src/main/java/Alex.java
+++ src/main/java/Alex.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Alex {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println("Hello! I'm Alex");
@@ -20,13 +20,29 @@ public class Alex {
                 } else {
                     System.out.println("    ____________________________________________________________");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println("    "+(i + 1) + ". " + tasks[i]);
+                        System.out.println("    "+(i + 1) + ".["+tasks[i].getStatusIcon()+"] " + tasks[i].description);
                     }
                     System.out.println("    ____________________________________________________________");
                 }
-            } else if (!input.equalsIgnoreCase("bye")) {
+            }else if (input.contains("unmark")) {
+                //unmarked placed before to prevent triggering on "mark"
+                System.out.println("    ____________________________________________________________");
+                int target = Integer.parseInt(input.substring(input.indexOf(" ")+ 1, input.length()));
+                tasks[target - 1].setUnmark();
+                System.out.println("    Unmarked "+tasks[target-1].description);
+                System.out.println("    ____________________________________________________________");
+            } 
+            else if (input.contains("mark")) {
+                System.out.println("    ____________________________________________________________");
+                int target = Integer.parseInt(input.substring(input.indexOf(" ")+ 1, input.length()));
+                tasks[target - 1].setMark();
+                System.out.println("    Marked "+tasks[target-1].description);
+                System.out.println("    ____________________________________________________________");
+            } 
+            else if (!input.equalsIgnoreCase("bye")) {
                 if(taskCount < 100){
-                    tasks[taskCount] = input;
+                    tasks[taskCount] = new Task(input);
+
                     taskCount++;
                     System.out.println("    ____________________________________________________________");
                     System.out.println("    added: " + input);
